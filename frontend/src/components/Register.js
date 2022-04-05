6bb840d08945a93bef01c5badb2e615ab906b278
c0ahja01 <C0ahja01@students.oamk.fi>
2022-04-05 20:09:25 +0300

    jee

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -57,7 +57,7 @@ res.json());
         </p>
             <form onSubmit={ addSubmit}>
                 <div><input type="text"  value = { username} placeholder='Käyttäjätunnus' className='loginInsertBox' onChange = {(e) => setUsername(e.target.value)}></input></div>
-                <div><input type="text"  value = { password }placeholder='Salasana' className='loginInsertBox' onChange = {(e) => setPassword(e.target.value)}></input></div>
+                <div><input type="password"  value = { password }placeholder='Salasana' className='loginInsertBox' onChange = {(e) => setPassword(e.target.value)}></input></div>
                 <div><input type="text" value ={ fname } placeholder='Etunimi' className='loginInsertBox' onChange = {(e) => setFname(e.target.value)}></input></div>
                 <div><input type="text" value ={ lname } placeholder='Sukunimi' className='loginInsertBox' onChange = {(e) => setLname(e.target.value)}></input></div>
                 <div><input type="text"  value ={ address } placeholder='Osoite' className='loginInsertBox' onChange = {(e) => setAddress(e.target.value)}></input></div>
